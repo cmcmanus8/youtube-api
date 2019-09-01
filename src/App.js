6e448edbb357111850d4bb12ec21e2f428d7d719
Calum McManus <calum.mcmanus@outlook.com>
2019-09-01 18:35:22 +0100

    Removed API Key

--- src/App.js
+++ src/App.js
@@ -25,7 +25,7 @@ class App extends React.Component {
             params: {
                 part: 'snippet',
                 maxResults: 5,
-                key: '[API Key]',
+                key: '[API KEY]',
                 q: searchTerm,
             }
         });
